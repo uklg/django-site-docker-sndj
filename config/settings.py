5f4a01dcd0b975693f9da89e56e35fc9c86760c7
Richard Murdoch <rmu@live.co.uk>
2022-06-30 19:15:57 +0100

    working css for django

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,7 @@ if DEBUG:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
+
+# Allows non debug css to work for /admin
+WHITENOISE_STATIC_PREFIX = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/' 

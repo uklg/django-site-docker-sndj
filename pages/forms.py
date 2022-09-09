0e4df6ea3fdf9a85faa5eda3ad92c00fbf59bc4f
Richard Murdoch <rmu@live.co.uk>
2022-09-09 10:07:59 +0100

    update form classes

--- pages/forms.py
+++ pages/forms.py
@@ -5,15 +5,15 @@ class ContactForm(forms.Form):
     submit_name = forms.CharField(label='',widget=forms.TextInput(attrs={
 							'placeholder':'Your Name',
 							#'style': 'width: 300px;',
-							'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+							'class': 'form-group mb-4'}), required=False)
     submit_email = forms.EmailField(label='',widget=forms.EmailInput(attrs={
                                                                 'placeholder':'Your Email',
                                                                 #'style': 'width: 300px;',
-                                                                'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+                                                                'class': 'form-group mb-4'}), required=False)
     submit_number = forms.CharField(label='',widget=forms.TextInput(attrs={
                                                                 'placeholder':'Your Number',
                                                                 #'style': 'width: 300px;',
-                                                                'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+                                                                'class': 'form-group mb-4'}), required=False)
 
 
 
@@ -21,4 +21,4 @@ class ContactForm(forms.Form):
 							'placeholder':'Your Message',
 							#'style':'width: 300px;',
                                                         'rows':6,
-							'class': 'form-control'}), required=False)
+							'class': 'form-group mb-0'}), required=False)

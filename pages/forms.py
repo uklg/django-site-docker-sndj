9d32f65bf01c6b857f58ea75b63f8a0fda1f4aad
Richard Murdoch <rmu@live.co.uk>
2022-09-09 10:19:36 +0100

    update form css

--- pages/forms.py
+++ pages/forms.py
@@ -5,15 +5,15 @@ class ContactForm(forms.Form):
     submit_name = forms.CharField(label='',widget=forms.TextInput(attrs={
 							'placeholder':'Your Name',
 							#'style': 'width: 300px;',
-							'class': 'form-group mb-4'}), required=False)
+							'class': 'form-control'}), required=False)
     submit_email = forms.EmailField(label='',widget=forms.EmailInput(attrs={
                                                                 'placeholder':'Your Email',
                                                                 #'style': 'width: 300px;',
-                                                                'class': 'form-group mb-4'}), required=False)
+                                                                'class': 'form-control'}), required=False)
     submit_number = forms.CharField(label='',widget=forms.TextInput(attrs={
                                                                 'placeholder':'Your Number',
                                                                 #'style': 'width: 300px;',
-                                                                'class': 'form-group mb-4'}), required=False)
+                                                                'class': 'form-control'}), required=False)
 
 
 
@@ -21,4 +21,4 @@ class ContactForm(forms.Form):
 							'placeholder':'Your Message',
 							#'style':'width: 300px;',
                                                         'rows':6,
-							'class': 'form-group mb-0'}), required=False)
+							'class': 'btn tm-send-btn tm-fl-right'}), required=False)
